260fc424a640ea1bebe787b85b00e9764d2e85f0
Joy-whale0321 <jingyuzhang21@m.fudan.edu.cn>
2024-12-20 14:11:15 +0800

    Update tutorial.cc

--- InttSeedingTrackDev/PythiaGen/physiTuto/tutorial.cc
+++ InttSeedingTrackDev/PythiaGen/physiTuto/tutorial.cc
@@ -1,4 +1,4 @@
-a//____________________________________________________________________________..
+//____________________________________________________________________________..
 //
 // This is a template for a Fun4All SubsysReco module with all methods from the
 // $OFFLINE_MAIN/include/fun4all/SubsysReco.h baseclass
